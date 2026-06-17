51e887aabe56916a802127225bf553befb75aff5
David Soldat <davidsoldat00@gmail.com>
2026-06-17 22:32:41 +0200

    fixing tests

--- backend/src/test/java/com/trucknest/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/trucknest/backend/BackendApplicationTests.java
@@ -1,10 +1,13 @@
 package com.trucknest.backend;
 
+import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
@@ -20,8 +23,13 @@ class BackendApplicationTests {
 	static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine");
 
 	@Container
-	@ServiceConnection
 	static RedisContainer redis = new RedisContainer("redis:7-alpine");
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", redis::getHost);
+		registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+	}
 
 	@Test
 	void contextLoads() {
